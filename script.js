ad98967d85e00144683aa96207b5bee9b37a2cea
PrashanthBalakrishnan <prashanthdilshaan@gmail.com>
2020-06-25 21:52:16 -0400

    Added a block and added some CSS to clock

--- script.js
+++ script.js
@@ -160,9 +160,36 @@ function removeLocalTodos(todo) {
     localStorage.setItem("todos", JSON.stringify(todos));
 }
 
-function addingClock() {
+
+function startTime() {
+    var today = new Date();
+    var h = today.getHours();
+    var m = today.getMinutes();
+    var s = today.getSeconds();
+    m = checkTime(m);
+    s = checkTime(s);
+    amOrPm = ''
+    if (h > 12) {
+        h -= 12;
+        amOrPm = ':PM';
+    } else {
+        amOrPm = ':AM';
+    }
 
 
+
+
+
+
+    document.getElementById('txt').innerHTML =
+        h + ":" + m + ":" + s + amOrPm;
+    var t = setTimeout(startTime, 500);
+}
+
+
+function checkTime(i) {
+    if (i < 10) { i = "0" + i };  // add zero in front of numbers < 10
+    return i;
 
 
 }
